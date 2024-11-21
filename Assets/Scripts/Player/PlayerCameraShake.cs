2629df36d8d8ead593b8bd004363bac0fa41221e
JovanAleksicFutureGames <145547495+JovanAleksicFutureGames@users.noreply.github.com>
2024-11-21 17:44:52 +0100

    Updated character controller
    Phase 1 char controller done. Moving on to weapons

--- Assets/Scripts/Player/PlayerCameraShake.cs
+++ Assets/Scripts/Player/PlayerCameraShake.cs
@@ -1,34 +1,54 @@
 using UnityEngine;
+using Math;
 
 namespace Characters.Player 
 {
     public class PlayerCameraShake : MonoBehaviour
     {
-        [SerializeField, Range(0.0f, 5.0f)] private float m_fIntensitity;
-        [SerializeField, Range(0.0f, 5.0f)] private float m_fFrequency;
-        [SerializeField, Range(0.0f, 10.0f)] private float m_sprintMultiplier = 2.0f; // 2 set as default. Range from 0 to 10;
+        [SerializeField, Range(0.0f, 5.0f)] private float m_fIntensitity = 0.1f; //default as 0.1. Range from 0.0f to 5.0f. Recommended value between 0.1f and 0.5f
+        [SerializeField, Range(0.0f, 100.0f)] private float m_fFrequency = 10.0f; //default as 10.0f, Range from 0.0f to 100.0f, Recommended value is at 10.0f;
+        [SerializeField, Range(0.0f, 10.0f)] private float m_fSprintMultiplier = 2.0f; // 2 set as default. Range from 0 to 10;
 
         private Vector3 m_vOriginalCameraPosition;
-        private Quaternion m_qOriginalCameraPosition;
+        private Quaternion m_qOriginalCameraRotation;
         private PlayerController m_playerController;
+        private float m_fShakeTimer;
 
+
+        #region Initialization
         private void Awake()
         {
-            m_vOriginalCameraPosition = Camera.main.transform.localPosition;
-            m_qOriginalCameraPosition = Camera.main.transform.localRotation;
             m_playerController = GetComponent<PlayerController>();
-
         }
+
+        private void Start()
+        {
+            m_fShakeTimer = 0.0f;
+            m_vOriginalCameraPosition = m_playerController.MainCamera.transform.localPosition;
+            m_qOriginalCameraRotation = m_playerController.MainCamera.transform.localRotation;
+        }
+        #endregion
 
         public void CameraShakeRun(Camera camera) 
         {
+
             if (m_playerController.IsSprinting)
             {
+                m_fShakeTimer += Time.deltaTime * m_fFrequency * m_fSprintMultiplier ;
+                float fXShake = Mathf.Sin(m_fShakeTimer) * m_fIntensitity;
+                float fZRotation = Mathf.Sin(m_fShakeTimer) * m_fIntensitity;
 
+                m_playerController.MainCamera.localPosition = m_vOriginalCameraPosition + new Vector3(fXShake, 0.0f, 0.0f);
+                m_playerController.MainCamera.localRotation = Quaternion.Euler(0.0f, 0.0f, fZRotation);
             }
             else if (!m_playerController.IsSprinting)
             {
-                 
+                m_fShakeTimer += Time.deltaTime * m_fFrequency;
+                float fXShake = Mathf.Sin(m_fShakeTimer) * m_fIntensitity;
+                float fZRotation = Mathf.Sin(m_fShakeTimer) * m_fIntensitity;
+
+                m_playerController.MainCamera.localPosition = m_vOriginalCameraPosition + new Vector3(fXShake, 0.0f, 0.0f);
+                m_playerController.MainCamera.localRotation = m_qOriginalCameraRotation * Quaternion.Euler(0.0f, 0.0f, fZRotation);
             }
 
 
@@ -46,7 +66,7 @@ namespace Characters.Player
 
         public void ReturnCameraToNormal(Camera camera) 
         {
-
+            m_playerController.MainCamera.localPosition = new Vector3(MathUtil.EaseOut(m_vOriginalCameraPosition.x), MathUtil.EaseOut(m_vOriginalCameraPosition.y), MathUtil.EaseOut(m_vOriginalCameraPosition.z));
         }
     }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -16,7 +18,6 @@ namespace Characters.Player
 
         #region Player Movement - On Land
         private Vector3 m_vMoveDirection;
-        [SerializeField] private float m_fJumpDelta = 10.0f;
         [SerializeField] private bool m_bIsInvertedYAxis = false;
         [SerializeField] private float m_fRotationSpeed = 30.0f;
         [SerializeField] private float m_fCurrentMoveSpeed;
@@ -40,21 +41,21 @@ namespace Characters.Player
         #region Gravity
         [SerializeField] private float m_fGravityValue = -9.81f;
         [SerializeField] private float m_fGroundedGravityValue = -2.0f;
+        [SerializeField] private float m_fFallingOffset = 3.0f; 
         #endregion
 
         #region Component Members
         private CharacterController m_cCharacterController;
         private InputSystem_Actions m_InputSystemActions;
         private PlayerCameraShake m_PlayerCameraShake;
+        #endregion
 
         #region Properties
-
         public bool IsSprinting => m_InputSystemActions.Player.Sprint.IsPressed();
         public Transform MainCamera => m_mainCamera.transform;
 
         #endregion
 
-        #endregion
         private void OnEnable()
         {
             if(m_InputSystemActions == null) 
@@ -104,31 +105,36 @@ namespace Characters.Player
             Crouch();
             Jump();
 
-            
 
-            if (m_bIsJumping) 
+            if (!m_bIsJumping)
             {
                 m_vMoveDirection.y = m_fGroundedGravityValue;
             }
-            else 
+            else
             {
                 m_vMoveDirection.y += m_fGravityValue * Time.deltaTime;
             }
 
-            m_cCharacterController.Move(new Vector3(m_vMoveDirection.x * m_fCurrentMoveSpeed * Time.deltaTime, m_vMoveDirection.y, m_vMoveDirection.z * m_fCurrentMoveSpeed * Time.deltaTime));
-            if(m_cCharacterController.velocity.magnitude != 0.0f)
-            {
-                m_PlayerCameraShake.CameraShakeRun(m_mainCamera);
-            }
-            else if(m_cCharacterController.velocity.magnitude <= float.Epsilon) 
-            {
-                //do nothing OR return to camera original pos
-            }
+            m_cCharacterController.Move(
+                new Vector3(
+                m_vMoveDirection.x * m_fCurrentMoveSpeed * Time.deltaTime,
+                m_vMoveDirection.y * Time.deltaTime * m_fFallingOffset,
+                m_vMoveDirection.z * m_fCurrentMoveSpeed * Time.deltaTime));
+
+
+            /*            if(m_cCharacterController.velocity.magnitude != 0.0f)
+                        {
+                            m_PlayerCameraShake.CameraShakeRun(m_mainCamera);
+                        }
+                        else if(m_cCharacterController.velocity.magnitude == 0.0f) 
+                        {
+                            m_PlayerCameraShake.ReturnCameraToNormal(m_mainCamera);
+                        }*/
         }
 
-        private void FixedUpdate()
+        private void FixedUpdate() 
         {
-            
+
         }
 
         private void MovePlayer() 
@@ -177,7 +183,7 @@ namespace Characters.Player
         {
             if (m_InputSystemActions.Player.Jump.WasPressedThisFrame() && m_cCharacterController.isGrounded && !m_bIsCrouching) 
             {
-                m_cCharacterController.Move(new Vector3(0.0f, Mathf.MoveTowards(m_vMoveDirection.y, Mathf.Sqrt(-2.0f * m_fJumpHeight * m_fGravityValue * Time.deltaTime), m_fJumpDelta)));
+                m_cCharacterController.Move(transform.up * Mathf.Sqrt(-2.0f * m_fJumpHeight * m_fGravityValue * m_fFallingOffset * Time.deltaTime));
                 Debug.Log("Jumping");
             }
         }
