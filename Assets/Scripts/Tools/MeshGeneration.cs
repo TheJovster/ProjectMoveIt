e94d261e41ac2f2caa7aee9c5e179182c63a88df
TheJovster <joca.251@hotmail.com>
2025-02-04 19:08:00 +0100

    Started MeshGeneration

--- Assets/Scripts/Tools/MeshGeneration.cs
+++ Assets/Scripts/Tools/MeshGeneration.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter))][RequireComponent(typeof(MeshRenderer))]
 public class MeshGeneration : MonoBehaviour
 {
+    [SerializeField, Range(3, 64)] private int numberOfSegments = 3;
     
 }
